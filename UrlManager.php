b86d27155b4b242a198b3130942d37ab65ed60c6
panchenkodv <lexx.klan@mail.ru>
2016-05-07 18:06:55 +0300

    Для корректного редиректа

--- UrlManager.php
+++ UrlManager.php
@@ -431,7 +431,7 @@ class UrlManager extends BaseUrlManager
             $url = rtrim($url, '/').'/';
         }
         Yii::trace("Redirecting to $url.", __METHOD__);
-        Yii::$app->getResponse()->redirect($url);
+        Yii::$app->getResponse()->redirect(urldecode($url));
         if (YII_ENV_TEST) {
             // Response::redirect($url) above will call `Url::to()` internally. So to really
             // test for the same final redirect URL here, we need to call Url::to(), too.
