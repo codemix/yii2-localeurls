785769af625d69430a11ec167b56eef47f933baf
Michael Härtl <haertl.mike@gmail.com>
2017-02-24 11:38:34 +0100

    Issue #91 Fix BC break with Yii < 2.0.10 due to normalizer

--- UrlManager.php
+++ UrlManager.php
@@ -170,7 +170,7 @@ class UrlManager extends BaseUrlManager
             }
             if ($process && !$this->_processed) {
                 // If a normalizer is configured, let it do it's job
-                if ($this->normalizer!==false) {
+                if (property_exists($this, 'normalizer') && $this->normalizer!==false) {
                     parent::parseRequest($request);
                 }
                 // Still here, so parent::parseRequest() didn't throw a UrlNormalizerRedirectException.
